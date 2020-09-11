2493a1c92babf4af5e38eb6cf98508346eb27b8e
Christopher Stanek <chris.stanek97@gmail.com>
2020-09-11 13:31:27 +0200

    Updated the grid to the right dimensions.

--- Constants.h
+++ Constants.h
@@ -7,27 +7,25 @@
 
 namespace MPCD {
 	namespace Constants {
-		constexpr int number = 500000;
+		constexpr int number = Grid::average_particles_per_cell * (Pipe::x_max / Grid::cell_dim) * (Pipe::y_max / Grid::cell_dim); // will be a func of Grid
 		constexpr int seed = 34534211;
-		constexpr double time_lapse = 0.1;
+		constexpr double time_lapse = 0.1; // will be a func of diff parameters
 		constexpr int timesteps = 1000;
 		namespace Pipe {
-			constexpr double x_0 = -1.0;
-			constexpr double y_0 = -1.0;
-			constexpr double x_max = 1.0;
-			constexpr double y_max = 1.0;
+			constexpr double x_0 = 0;
+			constexpr double y_0 = 0;
+			constexpr double x_max = 400;
+			constexpr double y_max = 20;
 			constexpr double width = x_max - x_0;
 			constexpr double height = y_max - y_0;
 			constexpr double aspect_ratio = width / height;
 		}
 		namespace Grid {
-			constexpr int average_particles_per_cell = 20;
+			constexpr int average_particles_per_cell = 10;
 			constexpr int num_cells = MPCD::Constants::number / average_particles_per_cell;
 			//constexpr int wanted_num_cells = min_num_cells * 2;
-			const double cell_dim = std::sqrt(MPCD::Constants::Pipe::width * MPCD::Constants::Pipe::height / num_cells );
-			const double max_shift = cell_dim / 2;
-			constexpr double grid_x_shift = MPCD::Constants::Pipe::x_0;
-			constexpr double grid_y_shift = MPCD::Constants::Pipe::y_0;
+			constexpr double cell_dim = 1;
+			constexpr double max_shift = cell_dim / 2;
 		}
 	}
 }
